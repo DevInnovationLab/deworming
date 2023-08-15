f5b7909b4498a36ef9de46a59cc43f6a2b5e15ed
Luiza Andrade <luiza.c.andrade@gmail.com>
2023-08-14 20:34:44 -0500

    made the last commit too early

--- code/13_compare_to_TMSDG.R
+++ code/13_compare_to_TMSDG.R
@@ -12,110 +12,95 @@ data <-
         estimates,
         prevalence,
         sep = "; "
+      ),
+    outcome =
+      factor(
+        outcome,
+        levels = c(
+          "Weight (kg)",
+          "MUAC (cm)",
+          "Height (cm)",
+          "Hemoglobin (g/dL)"
+        ),
+        ordered = TRUE
       )
   )
 
 
 data %>%
-  ggplot() %>%
-  geom_point(
+  ggplot(
     aes(
-      x = pe
+      y = source,
+      yend = source
     )
+  ) +
+  geom_vline(
+    xintercept = 0,
+    color = "black",
+    linetype = "dotted"
   ) +
   geom_segment(
     aes(
       x = ll95,
       xend = ul95
     ),
-    color = "grey50"
+    color = "grey50",
+    size = 2.5,
+    lineend = "round"
   ) +
   geom_segment(
     aes(
       x = ll90,
       xend = ul90
     ),
+    color = "black",
+    size = 2.5,
+    lineend = "round"
+  ) +
+  geom_text(
+    aes(
+      x = pe,
+      label = round(pe, 3)
+    ),
+    vjust = -1,
+    size = 3
+  ) +
+  geom_segment(
+    aes(
+      x = -.4,
+      xend = .4,
+      y = 8.57,
+      yend = 8.57
+    ),
     color = "black"
+  ) +
+  geom_segment(
+    aes(
+      x = -.4,
+      xend = .4,
+      y = .4,
+      yend = .4
+    ),
+    color = "black"
+  ) +
+  geom_point(
+    aes(
+      x = pe
+    ),
+    size = 4,
+    shape = 15
+  ) +
+  facet_wrap(
+    ~ outcome,
+    ncol = 4
+  ) +
+
+  theme_void() +
+  theme(
+    axis.text = element_text(size = 8),
+    axis.ticks.x = element_line(color = "black", size = .5),
+    axis.ticks.length.x = unit(.15, "cm"),
+    strip.text.x = element_text(size = 8, margin = margin(.25, .25, .25, .25, "cm")),
+    axis.text.y = element_text(hjust = 0),
+    strip.background = element_rect(color = "white")
   )
-
-######################
-# Set up forest plot
-######################
-#o <- "weight"
-o <- "height"
-
-for(o in c("weight", "height")){
-  #set min and max for plot
-  if (o=="weight"){
-    pe <- dta$weight_pe
-    se <- dta$weight_se
-  } else if (o=="height"){
-    pe <- dta$height_pe
-    se <- dta$height_se
-  }
-
-  lo       = min(pe - 1.96*se)*1.1
-  hi       = max(pe + 1.96*se)*1.1
-  len = hi-lo
-
-  #set up output file
-  if (o=="weight"){
-    thiswidth <- 600
-  } else if (o=="height"){
-    thiswidth <- 300
-  }
-  png(filename="output/figures/metaforest"%&% o %&% ".png", res=95, width=thiswidth, height=400,)
-  par(mar=c(4,6,0,2))
-
-  #do the forest plot
-  if (o=="weight"){
-    thisxlab <- "Effect of Deworming in kg"
-  } else if (o=="height"){
-    thisxlab <- "Effect of Deworming in cm"
-  }
-
-  leftedge  <- 1
-  rightedge <- 0
-  res1 <- rma(yi=pe, sei=se, method="DL", slab=dta$study)
-  if (o=="height"){
-    res1 <- rma(yi=pe, sei=se, method="DL", slab=c(""," "," "," "," "))
-    leftedge <- 0
-  }
-
-  if (o=="weight"){
-    forest(res1, addfit=FALSE, refline=0, xlim=c(lo-len*leftedge, hi+len*rightedge), alim=c(lo,hi),
-           xlab=thisxlab, lty="solid", bg="red", psize=0.6, level=5,
-           annotate=FALSE)
-  } else if (o=="height"){
-    forest(res1, addfit=FALSE, refline=0, xlim=c(lo-len*leftedge, hi+len*rightedge), alim=c(lo,hi),
-           xlab=thisxlab, lty="solid", bg="red", psize=0.6, level=5,
-           annotate=FALSE, slab=c(""," "," "," "," "))
-  }
-
-  #add 95% and 90% error bars
-  segments(pe-1.96*se, length(dta$study):1, pe+1.96*se, length(dta$study):1, col="grey50",lty=1, lwd=8)
-  segments(pe-1.65*se, length(dta$study):1, pe+1.65*se, length(dta$study):1, col="grey5",lty=1, lwd=8)
-
-  #add bigger point estimate marker
-  points(pe, length(dta$study):1, pch=15, cex=2)
-
-  #add effect size label
-  text(pe,length(dta$study):1,pe,pos=3)
-
-  #add column headers and legend
-  if (o=="weight") {
-    text( lo-len*leftedge        , length(dta$study)+1.5, "Study"               , pos=4)
-  }
-
-  dev.off()
-
-  #make a legend
-  png(filename="output/figures/metaforest_legend.png", res=95, width=1000, height=40)
-  par(mar=c(0,0,0,0))
-  plot(1,xlim=c(0,1800),ylim=c(0,100),type="n",axes=F)
-  rect(650, 15, 725, 85,col="grey5",border=NA)
-  text(800, 50, "95% CI", cex=1)
-  rect(1000, 15, 1075, 85,col="grey50",border=NA)
-  text(1150, 50, "90% CI",cex=1)
-  dev.off()
-}
